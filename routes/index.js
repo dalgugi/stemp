4836a5f8e0f702dd8566f28ec9ac1fb9064e9774
yj-oh <holidayoh99@gmail.com>
2019-11-11 22:32:03 +0900

    [기능] '/' 경로와 login.html 마운트
    [해결] #1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+/*
+main server js file
+*/
+var express = require('express');//express 모듈 가져오기
+var router = express.Router(); //express의 라우터를 router에 저장
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {//router를 실행하여 login.html 가져오기, login.html에 title 변수에 'Express' 값을 넣어서 res해줌
+  res.render('login.html', { title: 'S\'Tempress' });
 });
 
-module.exports = router;
+module.exports = router;//router 모듈 export
