9b26356297de40c3de5737d2c9b1f2ce95266ac2
intosy227 <intosy227@gmail.com>
2019-11-12 21:53:28 +0900

    [리펙토링] title 값 오타 수정 및 필요 없는 소스 삭제
    index.js의 title 값 오타 수정 및 error.html, index.html의 필요없는 소스정리

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router(); //express의 라우터를 router에 저장
 
 /* GET home page. */
 router.get('/', function(req, res, next) {//router를 실행하여 login.html 가져오기, login.html에 title 변수에 'Express' 값을 넣어서 res해줌
-  res.render('login.html', { title: 'S\'Tempress' });
+  res.render('login.html', { title: 'S\'Temp' });
 });
 
 module.exports = router;//router 모듈 export
